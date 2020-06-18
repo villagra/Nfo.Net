62dfe21a3e2a7ad062466416905ade79d054076a
Jose <villagra@gmail.com>
2020-06-18 17:54:12 +0200

    Initial commit

---
+++ Src/Nfo.Net/Model/IMedia.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nfo.Net.Model
+{
+    public interface IMedia
+    {
+    }
+}
---
+++ Src/Nfo.Net/NfoReader.cs
@@ -0,0 +1,15 @@
+﻿using Nfo.Net.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nfo.Net
+{
+    public class NfoReader
+    {
+        public IMedia Read(String content)
+        {
+            return null;
+        }
+    }
+}
