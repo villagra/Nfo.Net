5ee4e6b8c570447c2c24911d19258cf812bd1d2a
Jose <villagra@gmail.com>
2020-06-20 08:51:23 +0200

    Added tests project

---
+++ Src/Nfo.Net.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Nfo.Net.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void NfoReaderLinksFile1()
+        {
+            
+        }
+    }
+}
--- Src/Nfo.Net/NfoReader.cs
+++ Src/Nfo.Net/NfoReader.cs
@@ -1,15 +1,54 @@
-﻿using Nfo.Net.Model;
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Nfo.Net.Model;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Linq;
 
 namespace Nfo.Net
 {
     public class NfoReader
     {
-        public IMedia Read(String content)
+        public IMedia Read(String content, ILogger logger = null)
         {
+            logger = logger ?? NullLogger.Instance;
+            content = content.Trim('\r', '\n');
+
+            if (!content.StartsWith("<"))
+            {
+                //this should be a url nfo 
+            }
+            else
+            {
+                if (content.EndsWith(">"))
+                {
+                    ParseXml(content, logger);
+                }
+                else
+                {
+                    //this should be a mixed document
+                }
+            }            
+
             return null;
+        }
+
+        private void ParseXml(string content, ILogger logger = null)
+        {
+            //should be xml
+            try
+            {
+                XDocument xDocument = XDocument.Parse(content);
+            }
+            catch (System.Xml.XmlException ex)
+            {
+                logger.LogError(ex, "xml error");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "unkown error");
+            }
         }
     }
 }
